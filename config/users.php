21b6e879547a1bb6c0f3d14e07c9df53e82acc29
Allan Haggett <allan.haggett@gov.bc.ca>
2021-04-28 14:55:31 -0700

    Update config/users.php override to remove twitter and add working azure settings

--- config/users.php
+++ config/users.php
@@ -4,28 +4,25 @@ $config = ['Users' =>
                 ['Social' => 
                         ['login' => true]
                 ],
+                'Email' => [
+                        // determines if the user should include email
+                        'required' => true,
+                        // determines if registration workflow includes email validation
+                        'validate' => false,
+                    ],
                 'OAuth' => [
                         'providers' => [
-                                'twitter' => [
-                                        'service' => 'CakeDC\Auth\Social\Service\OAuth1Service',
-                                        'className' => 'League\OAuth1\Client\Server\Twitter',
-                                        'mapper' => 'CakeDC\Auth\Social\Mapper\Twitter',
+                                'azuread' => [
+                                        'service' => 'CakeDC\Auth\Social\Service\OAuth2Service',
+                                        'className' => 'TheNetworg\OAuth2\Client\Provider\Azure',
+                                        'mapper' => 'CakeDC\Auth\Social\Mapper\Azure',
                                         'options' => [
-                                                'redirectUri' => Router::fullBaseUrl() . '/auth/twitter',
-                                                'linkSocialUri' => Router::fullBaseUrl() . '/link-social/twitter',
-                                                'callbackLinkSocialUri' => Router::fullBaseUrl() . '/callback-link-social/twitter',
+                                                'userFields' => ['email', 'full_name'],
+                                                'redirectUri' => Router::fullBaseUrl() . '/auth/azuread',
+                                                'linkSocialUri' => Router::fullBaseUrl() . '/link-social/azuread',
+                                                'callbackLinkSocialUri' => Router::fullBaseUrl() . '/callback-link-social/azuread',
                                         ]
-                                ],
-                                // 'azuread' => [
-                                //         'service' => 'CakeDC\Auth\Social\Service\OAuth2Service', // noooo
-                                //         'className' => 'TheNetworg\OAuth2\Azure\Server\AzureAD', // noooo
-                                //         'mapper' => 'CakeDC\Auth\Social\Mapper\Azuread',         // noooo
-                                //         'options' => [
-                                //                 'redirectUri' => Router::fullBaseUrl() . '/auth/azuread',
-                                //                 'linkSocialUri' => Router::fullBaseUrl() . '/link-social/azuread',
-                                //                 'callbackLinkSocialUri' => Router::fullBaseUrl() . '/callback-link-social/azuread',
-                                //         ]
-                                // ]
+                                ]
                         ]
                         ],
             ];
