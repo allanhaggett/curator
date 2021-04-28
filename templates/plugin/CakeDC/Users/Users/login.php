1ef92f5d580c07725cebaa7bb1d3c56cf3bb1f1f
Allan Haggett <allan.haggett@gov.bc.ca>
2021-04-28 14:54:35 -0700

    Add login page override

---
+++ templates/plugin/CakeDC/Users/Users/login.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright 2010 - 2019, Cake Development Corporation (https://www.cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2010 - 2018, Cake Development Corporation (https://www.cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+?>
+<div class="container-fluid">
+<div class="row justify-content-md-center" id="colorful">
+<div class="col-6">
+<div class="p-5">
+
+<h1 class="display-4 mt-5">Access learning resources on demand</h1>
+<h2 class="font-weight-light">Sourced by BC Public Service curators</h2>
+<div class="p-3 rounded-lg mb-5 bg-white shadow-sm">
+<p style="font-size: 1.5rem">BC Public Service curators are trusted guides designing pathways of knowledge and 
+skill development. Learning Curator pathways may stand alone or they may supplement 
+your corporate training offered through the Learning Centre.</p>
+<p style="font-size: 1.3rem">A <a href="#">best practice in learning for organizations</a>, curation helps us develop 
+ourselves as part of a trusted, talented, and modern public service.</p>
+<div><a href="/auth/azuread" class="btn btn-lg btn-success">Sign In  with your.name@gov.bc.ca address to continue</a></div>
+</div>
+</div>
+</div>
